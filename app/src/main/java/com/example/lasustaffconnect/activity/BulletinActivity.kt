e0f2a448aaaf6f0b0228c4b4608104175b824608
Ola0x <rayjat51@gmail.com>
2023-12-04 21:59:37 +0100

    changed bulletin weblink

--- app/src/main/java/com/example/lasustaffconnect/activity/BulletinActivity.kt
+++ app/src/main/java/com/example/lasustaffconnect/activity/BulletinActivity.kt
@@ -1,12 +1,16 @@
 package com.example.lasustaffconnect.activity
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.KeyEvent
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import androidx.appcompat.app.AppCompatActivity
 import com.example.lasustaffconnect.R
 
+
 class BulletinActivity : AppCompatActivity() {
+
+    private lateinit  var webView: WebView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.fragment_webview)
@@ -20,17 +24,28 @@ class BulletinActivity : AppCompatActivity() {
 //        webview.getSettings().setDisplayZoomControls(false);
 
         // Find the WebView by its unique ID
-        val webView = findViewById<WebView>(R.id.webview_c)
+        this.webView = findViewById<WebView>(R.id.webview_c)
 
-        // loading http://www.google.com url in the WebView.
-        webView.loadUrl("http://35.153.50.50:8050/home/")
-
-        // this will enable the javascript.
-        webView.settings.javaScriptEnabled = true
-
-        // WebViewClient allows you to handle
+        val webSettings = webView.settings
         // onPageFinished and override Url loading.
         webView.webViewClient = WebViewClient()
 
+        // this will enable the javascript.
+        webSettings.javaScriptEnabled = true
+//        val webViewClient = WebViewClientImpl(this)
+//        webView.webViewClient = webViewClient
+
+        // loading http://www.google.com url in the WebView.
+        webView.loadUrl("https://lasuoer.online/publication.php")
+        // WebViewClient allows you to handle
+
+    }
+
+    override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
+        if (keyCode == KeyEvent.KEYCODE_BACK && this.webView.canGoBack()) {
+            this.webView.goBack()
+            return true
+        }
+        return super.onKeyDown(keyCode, event)
     }
 }
